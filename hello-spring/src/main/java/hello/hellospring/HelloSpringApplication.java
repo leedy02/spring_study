6ea18dcb61901179eea6d1e90cdd279bd1249bba
Lee Eui Seok <leedy02@users.noreply.github.com>
2022-09-10 02:39:49 +0900

    Spring study1
    MVC 모델을 이용한 회원 등록 페이지 만들기 (인프런 강의)

---
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,14 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//tomcat 내장 하고 있음
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
