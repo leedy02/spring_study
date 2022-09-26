b5c02090b76de6cc1be04c8d7ab6c468ab6571af
Lee Eui Seok <leedy02@users.noreply.github.com>
2022-09-26 19:22:47 +0900

    기존 member 값이 잘 들어가는지,  정상 동작하는지 확인하는 테스트 코드 개발

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -22,7 +22,7 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findByName(String name) {
-        store.values().stream()
+        return store.values().stream()
                 .filter(member->member.getName().equals(name))
                 .findAny();
     }
@@ -30,5 +30,9 @@ public class MemoryMemberRepository implements MemberRepository{
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
