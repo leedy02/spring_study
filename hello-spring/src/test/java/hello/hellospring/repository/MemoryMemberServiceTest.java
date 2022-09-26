2a5606397269c846d42794eac1ebe31ecd88a8b3
Lee Eui Seok <leedy02@users.noreply.github.com>
2022-09-27 00:31:30 +0900

    회원 가입 서비스 개발 및 테스트 코드 작성
    회원 가입 서비스 개발 및 테스트 모듈 개발
    - 각각 데이터 정상적으로 들어가는지 여부 테스트, 중복 여부 예외 처리 가능한지 테스트

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberServiceTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class MemoryMemberRepositoryTest {
+public class MemoryMemberServiceTest {
 
     MemberRepository repository  = new MemoryMemberRepository();
 
